bed800627502feaa8cd8a80211311a031f6dc44e
sawolej <kuroxian@gmail.com>
2022-08-26 00:23:51 +0200

    board improved

--- js/board_script.js
+++ js/board_script.js
@@ -3,13 +3,10 @@ function openSheet(x) {
   console.log("hehe open");
   var x = document.getElementById(x);
   if (x.style.visibility === 'hidden') {
-    //closeAll();
+    closeAll();
     x.style.visibility = 'visible';
     if(x==sheet1){
-        console.log("itis");
-        document.getElementById('insta').style.visibility = 'visable';
-        document.getElementById('fb').style.visibility = 'visable';
-        console.log("itis");
+
     }
   } else {
     x.style.visibility = 'hidden';
@@ -18,6 +15,5 @@ function openSheet(x) {
 
 function closeAll(){
     for (let i=1; i<5; i++) document.getElementById(`sheet${i}`).style.visibility = 'hidden';
-    if (butFb.style.visibility === 'visable') butFb.style.visibility = 'hidden';
-    if (butInsta.style.visibility === 'visable') butInsta.style.visibility = 'hidden';
+
 }
