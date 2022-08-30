3ef944bb4161679123b64bd16e6dbe94c2fbd6b7
NakerTheFirst <kcholysk@gmail.com>
2022-08-30 21:35:00 +0200

    Create level design

--- js/mainGame.js
+++ js/mainGame.js
@@ -25,89 +25,110 @@ window.addEventListener('load', function() {
   const input = new InputHandler();
   const background = new Background(canvas.width, canvas.height, -1920);
   const ground = new Ground(canvas.width, canvas.height, -1920);
-  const bigPlatform = new BigPlatform(canvas.width, canvas.height, 1250, 720);
-  const smallPlatform = new SmallPlatform(canvas.width, canvas.height, 700, 790);
-  const diskBehav = new Disk(canvas.width, canvas.height, "diskBehavioralImage", 500, 800);
-  const diskBio = new Disk(canvas.width, canvas.height, "diskBiologyImage", 600, 600);
-  const diskChad = new Disk(canvas.width, canvas.height, "diskChadImage", 300, 370);
-  const diskChem = new Disk(canvas.width, canvas.height, "diskChemistryImage", 900, 120);
-  const diskEksoc = new Disk(canvas.width, canvas.height, "diskEksocImage", 546, 756);
-  const diskGeo = new Disk(canvas.width, canvas.height, "diskGeographyImage", 762, 583);
-  const diskInter = new Disk(canvas.width, canvas.height, "diskInternationalImage", 845, 935);
-  const diskLaw = new Disk(canvas.width, canvas.height, "diskLawImage", 1205, 935);
-  const diskMaths = new Disk(canvas.width, canvas.height, "diskMathsImage", 2305, 500);
-  const diskManagement = new Disk(canvas.width, canvas.height, "diskManagementImage", 1505, 600);
-  const diskPhilology = new Disk(canvas.width, canvas.height, "diskPhilologyImage", 3205, 500);
-  const diskPhilosophy = new Disk(canvas.width, canvas.height, "diskPhilosophyImage", 1705, 500);
-  const diskTomaszow = new Disk(canvas.width, canvas.height, "diskTomaszowImage", 1905, 700);
-  const pc = new Pc(canvas.width, canvas.height, 2500, canvas.height - 242); 
+  const bigPlatform1 = new BigPlatform(canvas.width, canvas.height, 390, 715);
+  const bigPlatform2 = new BigPlatform(canvas.width, canvas.height, 1370, 715);
+  const bigPlatform3 = new BigPlatform(canvas.width, canvas.height, 2730, 715);
+  const bigPlatform4 = new BigPlatform(canvas.width, canvas.height, 4005, 751);
+  const bigPlatform5 = new BigPlatform(canvas.width, canvas.height, 4555, 565);
+  const bigPlatform6 = new BigPlatform(canvas.width, canvas.height, 5106, 378);
+  const bigPlatform7 = new BigPlatform(canvas.width, canvas.height, 8464, 565);
+  const smallPlatform1 = new SmallPlatform(canvas.width, canvas.height, 222, 455);
+  const smallPlatform2 = new SmallPlatform(canvas.width, canvas.height, 1620, 505);
+  const smallPlatform3 = new SmallPlatform(canvas.width, canvas.height, 1265, 345);
+  const smallPlatform4 = new SmallPlatform(canvas.width, canvas.height, 2285, 590);
+  const smallPlatform5 = new SmallPlatform(canvas.width, canvas.height, 2520, 380);
+  const smallPlatform6 = new SmallPlatform(canvas.width, canvas.height, 6034, 279);
+  const smallPlatform7 = new SmallPlatform(canvas.width, canvas.height, 6351, 389);
+  const smallPlatform8 = new SmallPlatform(canvas.width, canvas.height, 6668, 499);
+  const smallPlatform9 = new SmallPlatform(canvas.width, canvas.height, 6985, 610);
+  const smallPlatform10 = new SmallPlatform(canvas.width, canvas.height, 7302, 720);
+  const smallPlatform11 = new SmallPlatform(canvas.width, canvas.height, 8008, 755);
+  const smallPlatform12 = new SmallPlatform(canvas.width, canvas.height, 9095, 375);
+  const diskBehav = new Disk(canvas.width, canvas.height, "diskBehavioralImage", 264, 380);
+  const diskBio = new Disk(canvas.width, canvas.height, "diskBiologyImage", 1307, 270);
+  const diskChad = new Disk(canvas.width, canvas.height, "diskChadImage", 9640, 135);
+  const diskChem = new Disk(canvas.width, canvas.height, "diskChemistryImage", 1499, 640);
+  const diskEksoc = new Disk(canvas.width, canvas.height, "diskEksocImage", 2859, 625);
+  const diskGeo = new Disk(canvas.width, canvas.height, "diskGeographyImage", 3005, 224);
+  const diskInter = new Disk(canvas.width, canvas.height, "diskInternationalImage", 4134, 676);
+  const diskLaw = new Disk(canvas.width, canvas.height, "diskLawImage", 5235, 303);
+  const diskMaths = new Disk(canvas.width, canvas.height, "diskMathsImage", 519, 640);
+  const diskManagement = new Disk(canvas.width, canvas.height, "diskManagementImage", 7344, 645);
+  const diskPhilology = new Disk(canvas.width, canvas.height, "diskPhilologyImage", 6076, 204);
+  const diskPhilosophy = new Disk(canvas.width, canvas.height, "diskPhilosophyImage", 4850, 150);
+  const diskTomaszow = new Disk(canvas.width, canvas.height, "diskTomaszowImage", 6710, 424);
+  const pc = new Pc(canvas.width, canvas.height, 12390, 838); 
   const sun = new Sun(canvas.width, canvas.height);
   const mountains = new Mountains(canvas.width, canvas.height, -1920);
-  const palmLeft1 = new Palms(canvas.width, canvas.height, "palmLeftOneImage", 155, 265, 1600);
-  const palmLeft2 = new Palms(canvas.width, canvas.height, "palmLeftTwoImage", 165, 270, 3520);
-  const palmRight1 = new Palms(canvas.width, canvas.height, "palmRightOneImage", 185, 265, 175);
-  const palmRight2 = new Palms(canvas.width, canvas.height, "palmRightTwoImage", 120, 185, 1050);
-  
+  const palmLeftOne1 = new Palms(canvas.width, canvas.height, "palmLeftOneImage", 155, 265, 1600);
+  const palmLeftOne2 = new Palms(canvas.width, canvas.height, "palmLeftOneImage", 155, 265, 1600);
+  const palmLeftTwo1 = new Palms(canvas.width, canvas.height, "palmLeftTwoImage", 165, 270, 3520);
+  const palmLeftTwo2 = new Palms(canvas.width, canvas.height, "palmLeftTwoImage", 165, 270, 3520);
+  const palmLeftTwo3 = new Palms(canvas.width, canvas.height, "palmLeftTwoImage", 165, 270, 3520);
+  const palmLeftTwo4 = new Palms(canvas.width, canvas.height, "palmLeftTwoImage", 165, 270, 3520);
+  const palmRightOne1 = new Palms(canvas.width, canvas.height, "palmRightOneImage", 185, 265, 175);
+  const palmRightOne2 = new Palms(canvas.width, canvas.height, "palmRightOneImage", 185, 265, 175);
+  const palmRightOne3 = new Palms(canvas.width, canvas.height, "palmRightOneImage", 185, 265, 175);
+  const palmRightOne4 = new Palms(canvas.width, canvas.height, "palmRightOneImage", 185, 265, 175);
+  const palmRightTwo1 = new Palms(canvas.width, canvas.height, "palmRightTwoImage", 120, 185, 1050);
+  const palmRightTwo2 = new Palms(canvas.width, canvas.height, "palmRightTwoImage", 120, 185, 1050);
+  const palmRightTwo3 = new Palms(canvas.width, canvas.height, "palmRightTwoImage", 120, 185, 1050);
+  const palmRightTwo4 = new Palms(canvas.width, canvas.height, "palmRightTwoImage", 120, 185, 1050);
+  const palmRightTwo5 = new Palms(canvas.width, canvas.height, "palmRightTwoImage", 120, 185, 1050);
+
+  const disks = [diskBehav, diskBio, diskChad, diskChem, diskEksoc, diskGeo, diskInter, diskLaw, diskManagement, 
+    diskMaths, diskPhilology, diskPhilosophy, diskTomaszow];
+
+  const palms = [palmLeftOne1, palmLeftTwo1, palmRightOne1, palmRightTwo1];
+
+  const platforms = [smallPlatform1, smallPlatform2, smallPlatform3, smallPlatform4, smallPlatform5, 
+    smallPlatform6, smallPlatform7, smallPlatform8, smallPlatform9, smallPlatform10, smallPlatform11, 
+    smallPlatform12, bigPlatform1, bigPlatform2, bigPlatform3, bigPlatform4, bigPlatform5, bigPlatform6, 
+    bigPlatform7];
+
   // Main game loop - refresh every frame
   function animate() {
+
+    console.log(player.x);
     
-    // Draw graphics
+    // Draw background
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     background.draw(ctx);
     sun.draw(ctx);
     mountains.draw(ctx);
     ground.draw(ctx);
-    palmLeft1.draw(ctx);
-    palmLeft2.draw(ctx);
-    palmRight1.draw(ctx);
-    palmRight2.draw(ctx);
-    bigPlatform.draw(ctx);
-    bigPlatform.collide(player);
-    smallPlatform.draw(ctx);
-    smallPlatform.collide(player);
-    if (!diskBehav.isNear(player)) {diskBehav.drawGlow(ctx); diskBehav.draw(ctx);}
-    if (!diskBio.isNear(player)) {diskBio.drawGlow(ctx); diskBio.draw(ctx);}
-    if (!diskChad.isNear(player)) {diskChad.drawGlow(ctx); diskChad.draw(ctx);}
-    if (!diskChem.isNear(player)) {diskChem.drawGlow(ctx); diskChem.draw(ctx);}
-    if (!diskEksoc.isNear(player)) {diskEksoc.drawGlow(ctx); diskEksoc.draw(ctx);}
-    if (!diskGeo.isNear(player)) {diskGeo.drawGlow(ctx); diskGeo.draw(ctx);}
-    if (!diskInter.isNear(player)) {diskInter.drawGlow(ctx); diskInter.draw(ctx);}
-    if (!diskLaw.isNear(player)) {diskLaw.drawGlow(ctx); diskLaw.draw(ctx);}
-    if (!diskMaths.isNear(player)) {diskMaths.drawGlow(ctx); diskMaths.draw(ctx);}
-    if (!diskManagement.isNear(player)) {diskManagement.drawGlow(ctx); diskManagement.draw(ctx);}
-    if (!diskPhilology.isNear(player)) {diskPhilology.drawGlow(ctx); diskPhilology.draw(ctx);}
-    if (!diskPhilosophy.isNear(player)) {diskPhilosophy.drawGlow(ctx); diskPhilosophy.draw(ctx);}
-    if (!diskTomaszow.isNear(player)) {diskTomaszow.drawGlow(ctx); diskTomaszow.draw(ctx);}
+    
+    for (let i = 0; i < palms.length; ++i) {
+      palms[i].draw(ctx);
+    }
+
+    // Draw platforms
+    for (let i = 0; i < platforms.length; ++i) {
+      platforms[i].draw(ctx);
+      platforms[i].collide(player);
+    }
+
+    // Draw disks
+    for (let i = 0; i < disks.length; ++i) {
+      if (!disks[i].isNear(player)) {disks[i].drawGlow(ctx); disks[i].draw(ctx);}
+    }
+
     pc.draw(ctx);
     player.update(input.keys);
     player.draw(ctx);
 
-    // Only the one called platform will work for onGround
-    player.getPlatformInfo(bigPlatform);
+    // Only the one called platform will work for onGround - fix in the future
+    player.getPlatformInfo(bigPlatform1);
 
     // Side scrolling effect for moving rightwards
     if ((player.currentState === player.states[3] || (player.currentState === player.states[5] &&
       input.keys.d.pressed)) && player.x === 800) {
-      smallPlatform.x -= 10;
-      diskBehav.x -= 10;
-      diskBio.x -= 10;
-      diskChad.x -= 10;
-      diskChem.x -= 10;
-      diskEksoc.x -= 10;
-      diskGeo.x -= 10;
-      diskInter.x -= 10;
-      diskLaw.x -= 10;
-      diskMaths.x -= 10;
-      diskManagement.x -= 10;
-      diskPhilology.x -= 10;
-      diskPhilosophy.x -= 10;
-      diskTomaszow.x -= 10;
-      bigPlatform.x -= 10;
+      
+      for (let i = 0; i < disks.length; ++i) disks[i].x -= 10;
+      for (let i = 0; i < palms.length; ++i) palms[i].x -= 7;
+      for (let i = 0; i < platforms.length; ++i) platforms[i].x -= 10;
+
       background.x -= 0.05;
-      palmLeft1.x -= 7;
-      palmLeft2.x -= 7;
-      palmRight1.x -= 7;
-      palmRight2.x -= 7;
       ground.x -= 10;
       pc.x -= 10;
       mountains.x -= 5;
@@ -115,28 +136,13 @@ window.addEventListener('load', function() {
 
     // Side scrolling effect for moving leftwards
     else if ((player.currentState == player.states[2] || (player.currentState === player.states[4] &&
-      input.keys.a.pressed)) && 
-      player.x === 400) {
-        smallPlatform.x += 10;
-      diskBehav.x += 10;
-      diskBio.x += 10;
-      diskChad.x += 10;
-      diskChem.x += 10;
-      diskEksoc.x += 10;
-      diskGeo.x += 10;
-      diskInter.x += 10;
-      diskLaw.x += 10;
-      diskMaths.x += 10;
-      diskManagement.x += 10;
-      diskPhilology.x += 10;
-      diskPhilosophy.x += 10;
-      diskTomaszow.x += 10;
-      bigPlatform.x += 10;
+      input.keys.a.pressed)) && player.x === 400) {
+
+      for (let i = 0; i < disks.length; ++i) disks[i].x += 10;
+      for (let i = 0; i < palms.length; ++i) palms[i].x += 7;
+      for (let i = 0; i < platforms.length; ++i) platforms[i].x += 10;
+
       background.x += 0.05;
-      palmLeft1.x += 7;
-      palmLeft2.x += 7;
-      palmRight1.x += 7;
-      palmRight2.x += 7;
       ground.x += 10;
       pc.x += 10;
       mountains.x += 5;
@@ -147,7 +153,7 @@ window.addEventListener('load', function() {
     if (mountains.x <= -3840 || mountains.x >= 0) mountains.x = -1920;
     if (ground.x <= -3840 || ground.x >= 0) ground.x = -1920;
 
-    requestAnimationFrame(animate)
+    requestAnimationFrame(animate);
   }
 
   animate();
