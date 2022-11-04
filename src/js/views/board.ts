60f69bf1e7e46356d0bbeb0da7e10ce4baeb6b2e
NakerTheFirst <kcholysk@gmail.com>
2022-11-04 16:18:47 +0100

    Add board.js note outlines

--- src/js/views/board.ts
+++ src/js/views/board.ts
@@ -21,10 +21,10 @@ export const Board = {
   <div id="wrapper" class="board-wrapper">
     <div id="arrow" class="arrow-wrapper"></div>
     <img id="sheet1" src="./assets/pics/sheet1.png" class="undraggable">
-    <img id="sheet1click" src="./assets/pics/sheet1.png" class="undraggable">
-    <img id="sheet2click" src="./assets/pics/sheet1.png" class="undraggable">
-    <img id="sheet3click" src="./assets/pics/sheet1.png" class="undraggable">
-    <img id="sheet4click" src="./assets/pics/sheet1.png" class="undraggable">
+    <div id="sheet1click" class="undraggable"></div>
+    <div id="sheet2click" class="undraggable"></div>
+    <div id="sheet3click" class="undraggable"></div>
+    <div id="sheet4click" class="undraggable"></div>
     <img id="sheet2" src="./assets/pics/sheet2.png" class="undraggable">
     <img id="sheet3" src="./assets/pics/sheet3.png" class="undraggable">
     <img id="sheet4" src="./assets/pics/sheet4.png" class="undraggable">
