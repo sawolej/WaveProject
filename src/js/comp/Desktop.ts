32a3ed086f27bf91d94b9bae66352816041e3a40
NakerTheFirst <kcholysk@gmail.com>
2023-01-09 17:49:10 +0100

    Add initial desktop window dragging fix

--- src/js/comp/Desktop.ts
+++ src/js/comp/Desktop.ts
@@ -9,6 +9,11 @@ export const Desktop = class {
   shorter: HTMLButtonElement;
   cross: HTMLButtonElement;
   click: HTMLAudioElement;
+  elmnt: HTMLElement;
+  pos1: number;
+  pos2: number;
+  pos3: number;
+  pos4: number;
 
   constructor() {
     this.apps = glob.document.getElementById("os_apps") as HTMLElement
@@ -18,6 +23,13 @@ export const Desktop = class {
     this.maximise = glob.document.getElementById("maximise") as HTMLButtonElement
     this.shorter = glob.document.getElementById("shorter") as HTMLButtonElement
     this.cross = glob.document.getElementById("cross") as HTMLButtonElement
+
+    // Initialise elmnt as any element, since its changed on click anyway
+    this.elmnt = glob.document.getElementById("window-header") as HTMLElement
+    this.pos1 = 0;
+    this.pos2 = 0;
+    this.pos3 = 0;
+    this.pos4 = 0;
 
     /* Sound effects */
     this.click = new Audio("./src/assets/sounds/click.mp3")
@@ -45,9 +57,13 @@ export const Desktop = class {
     this.createApp("System Info", "./src/assets/pics/systemIcon.png", "system-info")
     this.createApp("Whats that?", "./src/assets/pics/gameIcon.png", "game");
 
+    // Return to room view on clicking shutdown
     (glob.document.getElementById('shutdown') as HTMLElement).onclick = () => glob.document.location.hash = "#room";
+    
+    // Make the window draggable
+    this.dragElement(document.getElementById("window")!);
   }
-
+  
   createApp(name: any, image: any, id: any) {
 
     const app = glob.document.createElement("div")
@@ -133,5 +149,38 @@ export const Desktop = class {
 
     this.osWindow.style.width = "60%"
     this.osWindow.style.height = "60%"
+  }
+
+  dragElement(elmnt: HTMLElement) {
+    if (document.getElementById(elmnt.id + "-header")) {
+      document.getElementById(elmnt.id + "-header")!.onmousedown = this.dragMouseDown;
+    } else {
+      elmnt.onmousedown = this.dragMouseDown;
+    }
+  }
+
+  dragMouseDown(e: any) {
+    e = e || window.event;
+    e.preventDefault();
+    this.pos3 = e.clientX;
+    this.pos4 = e.clientY;
+    document.onmouseup = this.closeDragElement;
+    document.onmousemove = this.elementDrag;
+  }
+
+  elementDrag(e: any) {
+    e = e || window.event;
+    e.preventDefault();
+    this.pos1 = this.pos3 - e.clientX;
+    this.pos2 = this.pos4 - e.clientY;
+    this.pos3 = e.clientX;
+    this.pos4 = e.clientY;
+    this.elmnt.style.top = (this.elmnt.offsetTop - this.pos2) + "px";
+    this.elmnt.style.left = (this.elmnt.offsetLeft - this.pos1) + "px";
+  }
+
+  closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
   }
 }
